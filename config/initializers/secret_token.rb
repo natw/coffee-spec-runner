36d38d269913f208aa4b31cf766292003670e9bc
Nat Williams <nat.williams@gmail.com>
2013-07-18 21:09:44 -0500

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoffeeSpecRunner::Application.config.secret_key_base = '01ebbeb9d662303f112e93f14adc47cfd9f62c65f6d6bc684127504d828308ee093419db344dcb048cdb20a2a2c80d2913da85cf5b67e7e25a65cda5143da48a'
